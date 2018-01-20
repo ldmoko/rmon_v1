44f16de6022bd4aaa2a3a71366e45091aeeee79d
微笑领悟_默默坚强 <ldmoko@googlemail.com>
2018-01-20 16:25:12 +0800

    Update config.py

--- rmon/rmon/config.py
+++ rmon/rmon/config.py
@@ -5,7 +5,7 @@ class DevConfig():
 
     DEBUG = 1
     SQLALCHEMY_TRACK_MODIFICATIONS = 0
-    path = os.path.join(os.getcwd(), 'rmon.db').replace('\\', '/')
+    path = os.path.join(os.getcwd(), 'rmon.db').replace('\\', '/') # 目的是把\\换成/，否则在windows下这种路径是非法的
     SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(path)
     TEMPLATES_AUTO_RELOAD = 1
     
